ae2d75d2fd9584c1e91676a8cca5229a8f37f662
HasanAnorov <anorovhasan@gmail.com>
2025-06-13 17:10:55 +0500

    working on login section

--- app/src/main/java/com/ierusalem/androrat/core/ui/MainActivity.kt
+++ app/src/main/java/com/ierusalem/androrat/core/ui/MainActivity.kt
@@ -7,26 +7,58 @@ import androidx.core.view.ViewCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.ierusalem.androrat.R
-import com.ierusalem.androrat.core.utils.getSystemDetails
-import com.ierusalem.androrat.core.utils.log
-import com.ierusalem.androrat.databinding.ActivityMainBinding
+import com.ierusalem.androrat.core.data.preferences.DataStorePreferenceRepository
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
+    @Inject
+    lateinit var dataStorePreferenceRepository: DataStorePreferenceRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
         ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, insets -> insets }
-        setContentView(binding.root)
-        log(
-            getSystemDetails()
-        )
+
+        // Setup the NavHostFragment and set the graph before setting content view
+        val navHostFragment = NavHostFragment.create(R.navigation.nav_graph)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment_container, navHostFragment)
+            .setPrimaryNavigationFragment(navHostFragment)
+            .commitNow()
+
+        // Set up navigation graph and start destination
+        setupNavigationGraph()
+
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun setupNavigationGraph() {
+        val navController = findNavController()
+        val navInflater = navController.navInflater
+        val navGraph = navInflater.inflate(R.navigation.nav_graph)
+
+        // Set the start destination synchronously based on login state
+        val startDestination = if (runBlocking { isLoginRequired() }) {
+            R.id.homeFragment
+        } else {
+            R.id.loginFragment
+        }
+
+        navGraph.setStartDestination(startDestination)
+        navController.graph = navGraph
+    }
+
+    private suspend fun isLoginRequired(): Boolean {
+        return withContext(Dispatchers.IO) {
+            dataStorePreferenceRepository.getLoginRequired.first()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -40,11 +72,6 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         return navHostFragment.navController
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
     }
 
 }
